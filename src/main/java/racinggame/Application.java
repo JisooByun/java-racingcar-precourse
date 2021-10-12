42ffdd303709f574667eec98fe55729e63f514ae
byun <bjs1616@gmail.com>
2021-10-12 21:03:48 +0900

    feat: RacingGame 객체 추가
    자동차들과 laps를 받은뒤 출력 및 startRace, flushOutput(현재 결과 출력) 추가

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,14 @@
 package racinggame;
 
+import nextstep.utils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 자동차 경주 게임 구현
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(, ) 기준으로 구분)");
+        String carNames = Console.readLine();
+        System.out.println("시도할 회수는 몇회인가요?");
+        String round = Console.readLine();
+        new RacingGame(new Cars(carNames), Integer.parseInt(round));
     }
 }
