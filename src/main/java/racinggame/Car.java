91ce223b6a1eed462e0988ade6a3073a93a6d641
byun <bjs1616@gmail.com>
2021-10-12 18:00:55 +0900

    feat: Car 객체 생성시 이름 없을 경우 Exception 발생
    Car 객체 생성할 때 이름이 "" 혹은 null일 경우 illegalArgumentException 발생한다.

--- src/main/java/racinggame/Car.java
+++ src/main/java/racinggame/Car.java
@@ -2,7 +2,14 @@ package racinggame;
 
 public class Car {
     public Car(String name) {
+        validateNameBlank(name);
         validateNameLength(name);
+    }
+
+    private void validateNameBlank(String name) {
+        if(name == null || "".equals(name)){
+            throw new IllegalArgumentException("자동차 이름은 공백일 수 없습니다. 1~5자 로 작성해주세요.");
+        }
     }
 
     private void validateNameLength(String name) {
--- src/test/java/racinggame/RacingGameTest.java
+++ src/test/java/racinggame/RacingGameTest.java
@@ -2,6 +2,7 @@ package racinggame;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RacingGameTest {
@@ -11,4 +12,13 @@ public class RacingGameTest {
          assertThatThrownBy(()->new Car("123456")).isInstanceOf(IllegalArgumentException.class).hasMessage("자동차 이름은 5자 이하로 작성해주세요.");
     }
 
+    @Test
+    void 자동차이름이_없으면_Exception_발생() {
+        assertThatThrownBy(()->new Car("")).isInstanceOf(IllegalArgumentException.class).hasMessage("자동차 이름은 공백일 수 없습니다. 1~5자 로 작성해주세요.");
+    }
+
+    @Test
+    void 자동차이름이_null이면_Exception_발생() {
+        assertThatThrownBy(()->new Car(null)).isInstanceOf(IllegalArgumentException.class).hasMessage("자동차 이름은 공백일 수 없습니다. 1~5자 로 작성해주세요.");
+    }
 }
