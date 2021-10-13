f0a472c36e618b02a4b392c5682c48e75596f731
byun <bjs1616@gmail.com>
2021-10-13 11:25:59 +0900

    fix: 에러메세지 출력
    에러메세지 출력하도록 변경
    
    (cherry picked from commit a696a58a1b62c7969d055757d7c8ebd741aa8dc0)

--- src/main/java/racinggame/InputConsole.java
+++ src/main/java/racinggame/InputConsole.java
@@ -77,6 +77,7 @@ public class InputConsole {
             validateLaps(laps);
             return true;
         }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
             return false;
         }
     }
